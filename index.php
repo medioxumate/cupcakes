db411dcea5cf43f77aa45a7fa8a19217781ce2b2
medioxumate <[briankiehn@comcast.net]>
2020-01-10 20:42:55 -0500

    added an index page. added html form

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created in PhpStorm
+ * @author Brian Kiehn
+ * @date 1/10/2020
+ * @version 2.0
+ * index.php
+ * GreenRiverDev
+ */
+
+?>
+
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+
+    <!--header-->
+    <h1>Cupcake Fundraiser</h1>
+
+    <!--Form begins:-->
+    <form action method = "post">
+
+    <!--Enter name-->
+    <p>Your name:</p>
+        <label>
+            <input type="text" name = "name"/>
+        </label><br>
+
+
+    <!--Cupcake options-->
+
+    <!--Submit option and end of form-->
+    <input type="submit" value="Order"/>
+    <input type="reset" value="Reset"/></form>
+
+    <!--end of html-->
+    </body>
+</html>
